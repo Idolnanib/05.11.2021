549b986b2601f1f43c43778d32d1abdcd6b16de4
Vladimir Zhirickii <vovanzhirickii@mail.ru>
2021-11-08 15:01:50 +0300

    Супер таблица умножения

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+
+in_put = int(input('Введи число , которое ты хочешь '))
+
 three = 1
 n = 1
 while n <= 10:
-    three = 3 * n
-    print('3 *', n, '= ', three)
+    three = in_put * n
+    print(in_put, ' *', n, '= ', three)
     n += 1
